ea3096deb50823b5b980256802b119b9642dfe4b
Jerry <yogeshlodhi1208@gmail.com>
2025-03-01 18:44:16 +0530

    Completed Authentication & added File Upload with Uploadthing

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,14 +1,28 @@
+"use client"
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from '~/utils/uploadthing';
 
 export function TopNav(){
+  const router = useRouter();
+
   return (
     <nav className="flex items-center justify-between p-4 text-xl font-semibold border-b">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
             <SignInButton/>
         </SignedOut>
         <SignedIn>
+            <UploadButton 
+              endpoint="imageUploader" 
+              onClientUploadComplete={() => {
+                // console.log("Upload complete in client")
+                router.refresh();
+              }}
+              // onUploadError={(error) => console.error("Upload Error:", error)}
+            />
             <UserButton/>
         </SignedIn>
       </div>
