1c8b896b060645e5308c3970e130db93cc07f33a
Jerry <yogeshlodhi1208@gmail.com>
2025-03-02 15:01:16 +0530

    Delete Button Added

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -6,16 +6,10 @@ export default function PhotoModal({
 } : {
     params: {id: string};
 }){
-    const id = Number(photoId);
-    if(Number.isNaN(id)){
-        console.log(photoId);
-        throw new Error("Invalid photo id");
-    }
-
     return (
         <Modal>
             {/* <img src={image.url} alt=""  className="w-96"/> */}
-            <FullPageImageView id={id}/>
+            <FullPageImageView photoId={photoId}/>
         </Modal>
     )
 }
