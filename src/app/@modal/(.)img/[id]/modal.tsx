8216a88e1b7d2fcd4a1fed79c63c1debd0fe082b
Jerry <yogeshlodhi1208@gmail.com>
2025-03-02 11:03:48 +0530

    modal added

---
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+import { type ElementRef, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export function Modal({children} : {children :  React.ReactNode}){
+    const router = useRouter();
+    const dialogRef = useRef<ElementRef<'dialog'>>(null);
+
+    useEffect(() => {
+        if(!dialogRef.current?.open){
+            dialogRef.current?.showModal();
+        }
+    }, []);
+
+    function onDismiss(){
+        router.back();
+    }
+
+    return createPortal(
+        <dialog 
+            ref={dialogRef} 
+            className="w-screen h-screen bg-black/50 text-white" 
+            onClose={onDismiss}
+        >
+            {children}
+            {/* <button onClick={onDismiss} className="close-button"/> */}
+        </dialog>,
+        // <div className="modal-backdrop">
+        // </div>,
+        document.getElementById("modal-root")!,
+    )
+}
